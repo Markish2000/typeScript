753ec1dc300b435456433ce51591b08dacc132db
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-14 14:52:08 -0300

    type finalizado

--- objetos/type.ts
+++ objetos/type.ts
@@ -2,20 +2,20 @@
   type Hero = {
     name: string;
     age?: number;
-    powers: string[];
+    powers: number[];
     getName?: () => string;
   };
 
   let flash: Hero = {
     name: 'Barry Allen',
     age: 24,
-    powers: ['Súper velocidad', 'Viajar en el tiempo'],
+    powers: [1, 2],
   };
 
   let superman: Hero = {
     name: 'Clark Kent',
     age: 60,
-    powers: ['Súper velocidad'],
+    powers: [1],
     getName() {
       return this.name;
     },
