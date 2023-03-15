08f8ddf9b0f67179d6a0ef4ff01403e0c64bd039
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-15 13:10:03 -0300

    union-types

--- objetos/type.js
+++ objetos/type.js
@@ -3,12 +3,12 @@
     let flash = {
         name: 'Barry Allen',
         age: 24,
-        powers: ['Súper velocidad', 'Viajar en el tiempo'],
+        powers: [1, 2],
     };
     let superman = {
         name: 'Clark Kent',
         age: 60,
-        powers: ['Súper velocidad'],
+        powers: [1],
         getName() {
             return this.name;
         },
