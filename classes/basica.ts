7c96ab0740de149596a55578b9c36a7d293c7f74
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-20 12:56:30 -0300

    Hacer más corto las clases

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,17 +1,14 @@
 (() => {
   class Avenger {
-    private name: string = 'No name';
-    private team: string = 'No team';
-    public realName?: string = 'no realName';
     static age: number = 35;
-    constructor(name: string, team: string, realName?: string) {
-      this.name = name;
-      this.team = team;
-      this.realName = realName;
-    }
+    constructor(
+      private name: string,
+      private team: string,
+      public realName?: string
+    ) {}
   }
 
-  const antMan: Avenger = new Avenger('AntMan', 'Capitan');
+  const antMan: Avenger = new Avenger('AntMan', 'Capitan', 'Scott Lang');
   // console.log(antMan);
   console.log(Avenger.age);
 })();
--- main.js
+++ main.js
@@ -2,16 +2,13 @@
 (() => {
     class Avenger {
         constructor(name, team, realName) {
-            this.name = 'No name';
-            this.team = 'No team';
-            this.realName = 'no realName';
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
     }
     Avenger.age = 35;
-    const antMan = new Avenger('AntMan', 'Capitan');
+    const antMan = new Avenger('AntMan', 'Capitan', 'Scott Lang');
     console.log(Avenger.age);
 })();
 //# sourceMappingURL=main.js.map
